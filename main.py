bfbde7eb3530122222efa42ce727ee8acdb52a7a
mdsyah79 <mdsyah79@gmail.com>
2023-05-30 17:30:44 +0800

    - final commit - completed Promodoro file

--- main.py
+++ main.py
@@ -12,12 +12,18 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
-def timer_reset():
+def reset_timer():
     global reps
+    global timer
 
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    tick_label.config(text="")
     reps = 0
 
 
@@ -27,24 +33,26 @@ def start_timer():
     global reps
     reps += 1
 
+    # original value is 60 for all the 1 below
     work_min_sec = WORK_MIN * 1
     short_break_sec = SHORT_BREAK_MIN * 1
     long_break_sec = LONG_BREAK_MIN * 1
 
     if reps % 8 == 0:
-        timer_label.config(text="Long Break", font=(FONT_NAME, 40, "bold"), fg=GREEN, bg=YELLOW)
+        timer_label.config(text="Long", fg=GREEN)
         count_down(long_break_sec)
     elif reps % 2 == 0:
-        timer_label.config(text="Short Break", font=(FONT_NAME, 40, "bold"), fg=RED, bg=YELLOW)
+        timer_label.config(text="Short", fg=RED)
         count_down(short_break_sec)
     else:
-        timer_label.config(text="Work", font=(FONT_NAME, 40, "bold"), fg=PINK, bg=YELLOW)
+        timer_label.config(text="Work", fg=PINK)
         count_down(work_min_sec)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     global reps
+    global timer
 
     count_min = math.floor(count / 60)
     count_sec = count % 60
@@ -53,9 +61,14 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        checkmarks = ""
+        no_of_checkmarks = math.floor(reps / 2)
+        for _ in range(0, no_of_checkmarks):
+            checkmarks += "✓"
+        tick_label.config(text=checkmarks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -72,13 +85,13 @@ canvas.grid(row=1, column=1)
 timer_label = Label(text="Timer", font=(FONT_NAME, 40, "bold"), fg=GREEN, bg=YELLOW)
 timer_label.grid(row=0, column=1)
 
-tick_label = Label(text="✓", font=(FONT_NAME, 20, "bold"), bg=YELLOW, fg=GREEN)
+tick_label = Label(text="", font=(FONT_NAME, 20, "bold"), bg=YELLOW, fg=GREEN)
 tick_label.grid(row=3, column=1)
 
 start_btn = Button(text="Start", highlightthickness=0, command=start_timer)
 start_btn.grid(row=2, column=0)
 
-reset_btn = Button(text="Reset", highlightthickness=0)
+reset_btn = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_btn.grid(row=2, column=2)
 
 window.mainloop()
